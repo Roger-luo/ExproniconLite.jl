590329b9d8078ae98ac3c81cfcaced25f87ca5a5
Roger-Luo <rogerluo.rl18@gmail.com>
2021-04-11 23:19:54 -0400

    export method

--- src/ExproniconLite.jl
+++ src/ExproniconLite.jl
@@ -10,7 +10,7 @@ export
     is_ifelse, is_for, is_field, is_field_default,
     split_function, split_function_head, split_struct,
     split_struct_name, split_ifelse, annotations,
-    uninferrable_typevars, has_symbol,
+    uninferrable_typevars, has_symbol, is_struct_not_kw_struct,
     is_literal, has_kwfn_constructor, has_plain_constructor,
     # transformations
     no_default, prettify, rm_lineinfo, flatten_blocks, name_only,
