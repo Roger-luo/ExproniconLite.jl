04bedb0ca781a22eb2dab9434e2716907112ba84
Roger-Luo <rogerluo.rl18@gmail.com>
2021-04-23 19:22:17 -0400

    generated from Expronicon

--- src/analysis.jl
+++ src/analysis.jl
@@ -345,12 +345,6 @@ begin
                 cache_9 = nothing
                 return_3 = nothing
                 x_51 = def
-                if x_51 isa JLFunction
-                    return_3 = let
-                            true
-                        end
-                    $(Expr(:symbolicgoto, Symbol("##final#322_1")))
-                end
                 if x_51 isa Expr
                     if begin
                                 if cache_9 === nothing
@@ -391,6 +385,12 @@ begin
                             end
                         $(Expr(:symbolicgoto, Symbol("##final#322_1")))
                     end
+                end
+                if x_51 isa JLFunction
+                    return_3 = let
+                            true
+                        end
+                    $(Expr(:symbolicgoto, Symbol("##final#322_1")))
                 end
                 return_3 = let
                         false
