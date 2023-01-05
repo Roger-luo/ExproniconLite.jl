bbbd090131cc98ccb5ab928dff8e24596db1e5d3
Roger-luo <rogerluo.rl18@gmail.com>
2023-01-05 17:12:37 -0500

    generated from Expronicon

--- src/analysis/compare.jl
+++ src/analysis/compare.jl
@@ -81,13 +81,27 @@
         begin
             true
             var"##407" = (lhs, rhs)
-            if var"##407" isa Tuple{TypeVar, TypeVar}
+            if var"##407" isa Tuple{Variable, Variable}
                 if begin
                             var"##408" = var"##407"[1]
-                            var"##408" isa TypeVar
+                            var"##408" isa Variable
                         end && begin
                             var"##409" = var"##407"[2]
-                            var"##409" isa TypeVar
+                            var"##409" isa Variable
+                        end
+                    var"##return#405" = begin
+                            return true
+                        end
+                    $(Expr(:symbolicgoto, Symbol("####final#406#456")))
+                end
+            end
+            if var"##407" isa Tuple{TypeVar, TypeVar}
+                if begin
+                            var"##410" = var"##407"[1]
+                            var"##410" isa TypeVar
+                        end && begin
+                            var"##411" = var"##407"[2]
+                            var"##411" isa TypeVar
                         end
                     var"##return#405" = begin
                             compare_expr(m, lhs.lb, rhs.lb) || return false
@@ -99,11 +113,11 @@
             end
             if var"##407" isa Tuple{LineNumberNode, LineNumberNode}
                 if begin
-                            var"##410" = var"##407"[1]
-                            var"##410" isa LineNumberNode
+                            var"##412" = var"##407"[1]
+                            var"##412" isa LineNumberNode
                         end && begin
-                            var"##411" = var"##407"[2]
-                            var"##411" isa LineNumberNode
+                            var"##413" = var"##407"[2]
+                            var"##413" isa LineNumberNode
                         end
                     var"##return#405" = begin
                             return true
@@ -113,28 +127,14 @@
             end
             if var"##407" isa Tuple{GlobalRef, GlobalRef}
                 if begin
-                            var"##412" = var"##407"[1]
-                            var"##412" isa GlobalRef
+                            var"##414" = var"##407"[1]
+                            var"##414" isa GlobalRef
                         end && begin
-                            var"##413" = var"##407"[2]
-                            var"##413" isa GlobalRef
+                            var"##415" = var"##407"[2]
+                            var"##415" isa GlobalRef
                         end
                     var"##return#405" = begin
                             return lhs === rhs
-                        end
-                    $(Expr(:symbolicgoto, Symbol("####final#406#456")))
-                end
-            end
-            if var"##407" isa Tuple{Variable, Variable}
-                if begin
-                            var"##414" = var"##407"[1]
-                            var"##414" isa Variable
-                        end && begin
-                            var"##415" = var"##407"[2]
-                            var"##415" isa Variable
-                        end
-                    var"##return#405" = begin
-                            return true
                         end
                     $(Expr(:symbolicgoto, Symbol("####final#406#456")))
                 end
